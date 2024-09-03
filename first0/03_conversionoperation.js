58b6dcbe9bf06c176f6e1668aed31b7e0af4665f
Mohammad Shad <sk9023718@gmail.com>
2024-09-03 17:16:34 +0000

    onversionoperation.js

--- first0/03_conversionoperation.js
+++ first0/03_conversionoperation.js
@@ -1,9 +1,9 @@
 let score =true
-console.log(typeof score);
+//console.log(typeof score);
 
 let valueInNumber =Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
 
 
 // "33"  --> 33
@@ -15,8 +15,47 @@ console.log(valueInNumber);
 let someNumber= 45
 
 let stringNumber=String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+
+//*********** operations********** *//
+let value =3
+let negvalue = -value
+console.log(negvalue);
+
+//console.log(2+2);
+//console.log(3-3);
+///console.log(2**3);
+///console.log(10/2);
+
+ let str1="hello "
+ let str2="shad"
+ let str3=str1+str2
+ console.log(str3);
+
+ console.log("1"+2);
+ console.log(1+"2");                                                            
+
+ 
+ console.log(true);
+ console.log(+true);// 1
+ console.log(+""); //0
+
+ let gamecenter=100
+ gamecenter++ //prefixor postfix
+ console.log(gamecenter);
+ 
+ 
+ 
+ 
+ 
+
+
+
+
+
+
 
 
 
